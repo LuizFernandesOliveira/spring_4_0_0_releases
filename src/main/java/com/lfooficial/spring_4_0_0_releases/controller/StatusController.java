3ba7f8ee51b87736fe1e83e2594b0fc2ac95fa4b
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2025-11-23 18:52:04 -0300

    init: initial commit

--- src/main/java/com/lfooficial/spring_4_0_0_releases/controller/StatusController.java
+++ src/main/java/com/lfooficial/spring_4_0_0_releases/controller/StatusController.java
@@ -8,11 +8,11 @@ public class StatusController {
 
   @GetMapping(path = "/status", version = "1.0")
   public String status() {
-    return "Application is running! In verwion 1.0";
+    return "Application is running! In version 1.0";
   }
 
   @GetMapping(path = "/status", version = "2.0")
   public String status2() {
-    return "Application is running! In verwion 2.0";
+    return "Application is running! In version 2.0";
   }
 }
